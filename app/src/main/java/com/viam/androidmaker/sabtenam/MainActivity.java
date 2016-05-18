1ea5bbeb7b7b78e124df4e3c463bcafc233dc2fc
viam <zvm4zvm@gmail.com>
2016-05-18 14:43:32 +0430

    afzudane checkbox va buttom

--- app/src/main/java/com/viam/androidmaker/sabtenam/MainActivity.java
+++ app/src/main/java/com/viam/androidmaker/sabtenam/MainActivity.java
@@ -2,7 +2,11 @@ package com.viam.androidmaker.sabtenam;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,34 @@ public class MainActivity extends AppCompatActivity {
         sb.append(getString(R.string.ghavanin));
 
         tv.setText(sb.toString());
+
+
+    }
+
+    public void itemClicked(View v) {
+
+
+            //code to check if this checkbox is checked!
+
+        CheckBox checkBox = (CheckBox)v;
+
+        if (((CheckBox) v).isChecked()) {
+            Toast.makeText(MainActivity.this,
+                    "accept rules", Toast.LENGTH_LONG).show();
+
+            Button btn = (Button) findViewById(R.id.button);
+
+            btn.setVisibility(View.VISIBLE);
+
+
+        }
+    }
+
+    public void register(View view) {
+
+
+        setContentView(R.layout.register);
+
+
     }
 }
